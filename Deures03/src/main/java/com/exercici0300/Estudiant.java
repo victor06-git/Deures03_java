24d8895dc4b4f6a6d490e55f9b28f55bf7819afb
victor06-git <asensiovector@gmail.com>
2025-02-14 09:46:59 +0100

    exercici0300

--- Deures03/src/main/java/com/exercici0300/Estudiant.java
+++ Deures03/src/main/java/com/exercici0300/Estudiant.java
@@ -1,18 +1,24 @@
 package com.exercici0300;
 
 public class Estudiant {
-    
+
+    private String nom;
+    private String id;
+    private static int comptadorEstudiants = 0;
+    private static final int MAX_ESTUDIANTS = 5;
     // Constructor
     public Estudiant(String nom, String id) {
-        
+        this.nom = nom;
+        this.id = id;
+        comptadorEstudiants++;
     }
 
     // Mètodes públics estàtics
     public static int getComptadorEstudiants() {
-        return -1;
+        return comptadorEstudiants;
     }
 
     public static boolean hiHaCapacitat() {
-        return false;
+        return comptadorEstudiants < MAX_ESTUDIANTS;
     }
 }
